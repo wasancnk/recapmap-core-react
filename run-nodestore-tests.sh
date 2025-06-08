cd182dce80f00db3443b94b6cb87a3936dc989d9
wasancnk <wasancnk.business@gmail.com>
2025-06-08 15:23:03 +0700

    Remove unused files

--- run-nodestore-tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Running nodeStore tests..."
-cd "d:/workspace_recapmap/recapmap-core-react/recapmap-core-react"
-npx vitest run src/__tests__/stores/nodeStore.test.ts --reporter=verbose
